aaf04b7e9fdc16e10c0a88b478812ae5aefa7486
ashish.g.verma <ashish.g.verma@accenture.com>
2018-01-16 12:43:00 +0530

    Added flash message feature + event and contact form data save -> DB

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const Session = require('express-session');
+const flash = require('connect-flash');
 require('dotenv').config({ path: 'variables.env' });
 const firebase = require('./firebase/firebase');
 
@@ -15,6 +18,22 @@ app.set('view engine', 'ejs') // register the template engine
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
+app.use(Session({ 
+  secret: 'Some Secret',
+  key: 'asdflxsdfj',
+  resave: false,
+  saveUninitialized: false
+ }))
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.flash = req.flash();
+    next();
+})
+
 app.get('/', require('./controller/homeController'));
 
 app.use('/login', require('./controller/loginController'));
@@ -23,7 +42,7 @@ app.use('/nonprofit', require('./controller/nonProfitController'));
 
 app.use('/contact',firebase.getFirebaseConn, require('./controller/contactController'));
 
-app.use("/events", require('./controller/eventController'));
+app.use("/events",firebase.getFirebaseConn, require('./controller/eventController'));
 
 app.use('/about', require('./controller/aboutUsController'));
 
--- controller/eventController.js
+++ controller/eventController.js
@@ -3,13 +3,32 @@
  */
 
 const router = require('express').Router();
+const firebaseServices = require('../services/firebaseServices');
+
+
+router.get("/", function (req, res) {
+    res.render("pages/eventList", { activePage: false });
+});
+
+router.get("/add", function (req, res) {
+    res.render("pages/editEvent", { activePage: false });
+});
+
+router.post('/add', async (req, res) => {
+    //if no validation errors the push to DB
+    try {
+        await firebaseServices.push(req.firebase, 'events', req.body);
+        req.flash('success', 'Thanks for Your valuable feedback!!');
+        res.redirect('/contact');
+    } catch(err) {
+        req.flash('error', 'Somthing bad happened! Please Try again!');
+        res.redirect('/contact');
+    }
+})
 
 router.get("/edit", function (req, res) {
     res.render("pages/editEvent", { activePage: false });
 });
 
-router.get("/list", function (req, res) {
-    res.render("pages/eventList", { activePage: false });
-});
 
 module.exports = router;
